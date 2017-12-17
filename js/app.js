e0f68aff75b44a2b85e54b1ef6b89947e7138204
betsy18 <betsy_dessire@hotmail.com>
2017-12-17 04:20:04 -0500

    Modificando el js

--- js/app.js
+++ js/app.js
@@ -5,14 +5,17 @@ window.addEventListener('load', function(event) {
     var btnRegister = document.getElementById('btnRegister');
     var btnRed = document.getElementById('btn-red');
     var callGreen = document.getElementById('call-green');
-
+    // var usuarioFirst = document.getElementById('usuario-first');
+    var usuarioSecond = document.getElementById('usuario-second');
     var main = document.getElementById('main');
     var call = document.getElementById('call');
-    var usuarioFirst = document.getElementById('usuario-first')
+    
 
     btnRegister.addEventListener('click', hide);
     btnRed.addEventListener('click', show);
     callGreen.addEventListener('click', firstForm);
+    usuarioSecond.addEventListener('click', secondForm);
+
 });
   
 function hide() {
@@ -28,6 +31,17 @@ function show() {
 }
 
 function firstForm() {
-    usuarioFirst.classList.toggle('hide');
+    alert('hola');
+    // event.preventDefault(event);
     call.classList.toggle('hide');
+    var usuarioFirst = document.getElementById('usuario-first');
+    usuarioFirst.classList.toggle('hide');
 }
+
+function secondForm() {
+    call.classList.toggle('hide');
+    var usuarioSecond = document.getElementById('usuario-second');
+    usuarioFirst.classList.toggle('hide');
+    usuarioSecond.classList.toggle('hide');
+}
+
