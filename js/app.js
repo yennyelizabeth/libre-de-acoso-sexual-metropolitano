ad24ed35f4a9cccaf1b94dc4948eeb2b5496536f
betsy18 <betsy_dessire@hotmail.com>
2017-12-16 22:51:25 -0500

    Actualizando la section-registrer

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 /* Funcionalidad */
+window.addEventListener('load', function(event) {
+    var btnRegister = document.getElementById('btnRegister');
+    btnRegister.addEventListener('click', display);
+});
+  
+function display() {
+    var sectionRegister = document.getElementById('sectionRegister');
+    sectionRegister.classList.add('display');
+}
