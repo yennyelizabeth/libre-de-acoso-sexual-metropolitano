00a9c710969283a319907a98f32c3b85482fb9b5
SteffAhv <tefavv22@gmail.com>
2017-12-17 01:41:00 -0500

    añade vista llamada

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 /* Funcionalidad */
 window.addEventListener('load', function(event) {
+    event.preventDefault(event);
     var btnRegister = document.getElementById('btnRegister');
     var main = document.getElementById('main');
+    var call = document.getElementById('call');
+    var btnRed = document.getElementById('btn-red');
     btnRegister.addEventListener('click', display);
+    btnRed.addEventListener('click', show)
 });
   
 function display() {
+    event.preventDefault(event);
     var sectionRegister = document.getElementById('sectionRegister');
     sectionRegister.classList.add('display');
     main.classList.remove('display');
+}
+function show() {
+    call.classList.toggle('display');
+    main.classList.toggle('display');
 }
