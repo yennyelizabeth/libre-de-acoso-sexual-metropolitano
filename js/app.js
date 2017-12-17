1f05bc801eca1c75ab36a1526958a6979c5c0f60
betsyvies <ing.betsyvies@gmail.com>
2017-12-17 02:43:51 -0500

    añadiendo vistas de usuario

--- js/app.js
+++ js/app.js
@@ -1,21 +1,33 @@
 /* Funcionalidad */
 window.addEventListener('load', function(event) {
     event.preventDefault(event);
+    
     var btnRegister = document.getElementById('btnRegister');
+    var btnRed = document.getElementById('btn-red');
+    var callGreen = document.getElementById('call-green');
+
     var main = document.getElementById('main');
     var call = document.getElementById('call');
-    var btnRed = document.getElementById('btn-red');
-    btnRegister.addEventListener('click', display);
-    btnRed.addEventListener('click', show)
+    var usuarioFirst = document.getElementById('usuario-first')
+
+    btnRegister.addEventListener('click', hide);
+    btnRed.addEventListener('click', show);
+    callGreen.addEventListener('click', firstForm);
 });
   
-function display() {
+function hide() {
     event.preventDefault(event);
     var sectionRegister = document.getElementById('sectionRegister');
-    sectionRegister.classList.add('display');
-    main.classList.remove('display');
+    sectionRegister.classList.add('hide');
+    main.classList.remove('hide');
 }
+
 function show() {
-    call.classList.toggle('display');
-    main.classList.toggle('display');
+    call.classList.toggle('hide');
+    main.classList.toggle('hide');
+}
+
+function firstForm() {
+    usuarioFirst.classList.toggle('hide');
+    call.classList.toggle('hide');
 }
